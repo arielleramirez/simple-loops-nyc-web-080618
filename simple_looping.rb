19d843eea3455dd56e5a9ef5ca95dbe6fa260296
Arielle N Ramirez <ariellenramirez@gmail.com>
2018-07-25 14:58:20 +0000

    automatically backed up by learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -1,15 +1,6 @@
 # REMEMBER: print your output to the terminal using 'puts'
 
 def loop_iterator(number_of_times)
-   z = 0
-  loop do
-    z+=1
-    puts "Welcome to Flatiron School's Web Development Course!"
-    break if z == number_of_times
-  end
-end
-   
-    
   # Code your solution here using the "loop" keyword to puts out the below phrase
   # This and all the other methods take an argument of an integer 
   # The integer is the number of times the loops should puts out the phrase
@@ -18,6 +9,14 @@ end
   # Maybe we should keep count of the number of times we've puts out the 
   #  phrase and break when the counter hits the appropriate number...
  # phrase = "Welcome to Flatiron School's Web Development Course!"
+   z = 0
+  loop do
+    z+=1
+    puts "Welcome to Flatiron School's Web Development Course!"
+    break if z == number_of_times
+  end
+end
+   
 
 
 def times_iterator(number_of_times)
@@ -28,19 +27,19 @@ def times_iterator(number_of_times)
 end
 
 def while_iterator(number_of_times)
-  a=0
+  a = 0
   while a < 7
     puts "Welcome to Flatiron School's Web Development Course!"
-    a+=1
+    a += 1
   end
 end
 
 def until_iterator(number_of_times)
-   b=7
+   b = 7
   until b == 0
     puts "Welcome to Flatiron School's Web Development Course!"
 
-    b-=1
+    b -= 1
   end
 end
 
@@ -49,7 +48,7 @@ def for_iterator(number_of_times)
   # remember that `for` requires a range of numbers. How can we turn our number into a range? c=7
   for c in 1..7 do
     puts "Welcome to Flatiron School's Web Development Course!"
-    c-=1
+    c -= 1
   end
 end
 
