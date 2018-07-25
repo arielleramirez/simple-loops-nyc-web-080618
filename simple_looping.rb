a203b8d55ce03327915a1f5eea20064d61f0e43e
Arielle N Ramirez <ariellenramirez@gmail.com>
2018-07-25 14:54:03 +0000

    automatically backed up by learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -24,10 +24,8 @@ def times_iterator(number_of_times)
   def times_iterator(number_of_times)
   # code your solution here using the "times" keyword
   7.times do
-    puts "Hello Ruby-007!"
+    puts "Welcome to Flatiron School's Web Development Course!"
   end
-end
-  phrase = "Welcome to Flatiron School's Web Development Course!"
   
 end
 
